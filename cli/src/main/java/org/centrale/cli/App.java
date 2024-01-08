9a67b5b43e291e058fb40794faffdb91215b8b71
Cledins <cassandra49130@gmail.com>
2024-01-08 10:07:16 +0100

    chifoumi terminé

--- cli/src/main/java/org/centrale/cli/App.java
+++ cli/src/main/java/org/centrale/cli/App.java
@@ -1,17 +1,52 @@
 package org.centrale.cli;
 
+import org.centrale.domain.Ciseaux;
+import org.centrale.domain.Feuille;
+import org.centrale.domain.Hand;
+import org.centrale.domain.Pierre;
+
 import java.util.Scanner;
 
 public class App {
-
     public static void main(String[] args) {
-        System.out.println("welcome to the app");
+        System.out.println("Jouons au Chifoumi !");
+        Scanner scYou = new Scanner(System.in);
+        System.out.println("Tapez 1 pour pierre, 2 pour feuille, 3 pour ciseaux :");
+        int yourChoice = scYou.nextInt();
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Veuillez saisir votre choix");
-        int choix = sc.nextInt();
-        System.out.println(choix);
+        Hand yourPick;
+
+        if (yourChoice == 1){
+            yourPick = new Pierre();
+        } else if (yourChoice == 2){
+            yourPick = new Feuille();
+        } else {
+            yourPick = new Ciseaux();
+        }
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        Scanner scMe = new Scanner(System.in);
+        System.out.println("A mon tour de taper 1 pour pierre, 2 pour feuille, 3 pour ciseaux :");
+        int myChoice = scMe.nextInt();
+
+        Hand myPick;
+
+        if (myChoice == 1){
+            myPick = new Pierre();
+        } else if (myChoice == 2){
+            myPick = new Feuille();
+        } else {
+            myPick = new Ciseaux();
+        }
+        System.out.println("(1=pierre,2=feuille,3=ciseaux) Vous avez choisi : " + yourChoice + " et j'ai choisi " + myChoice + " donc...\n");
+        int resultat = myPick.playWith(yourPick);
+
+        if (resultat==1){
+            System.out.println("Vous avez gagné !");
+        } else if (resultat==0){
+            System.out.println("Egalité !");
+        } else if (resultat==-1){
+            System.out.println("J'ai gagné !");
+        }
 
     }
-
 }
